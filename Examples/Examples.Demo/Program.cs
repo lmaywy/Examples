4deee2019042604eb1e113a3ec61faafc5827ca6
may.li <may.li@bleum.com>
2015-12-18 14:19:47 +0800

    test

---
+++ Examples/Examples.Demo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Examples.DesignPatterns.Obeserver;
+//using Examples.DesignPatterns.Singleton;
+
+namespace Examples.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------Desing Patterns Demo Start----------------");
+            ObserverPatternDemo.Demo();
+            //SingletonPatternDemo.SimpleSingletonDemo(); 
+            Console.WriteLine("------------- ----Desing Patterns Demo End-----------------");
+            Console.Read();
+        }
+    }
+}
