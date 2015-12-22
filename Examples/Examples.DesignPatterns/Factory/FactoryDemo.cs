fa625f0662399757cd8c77bb5d7f19acf7d3a3fd
may <maysaber@126.com>
2015-12-22 21:16:49 +0800

    [Dev] Factory Pattern- resolve the create instance issue using app.config.

--- Examples/Examples.DesignPatterns/Factory/FactoryDemo.cs
+++ Examples/Examples.DesignPatterns/Factory/FactoryDemo.cs
@@ -6,14 +6,20 @@ namespace Examples.DesignPatterns.Factory
 {
     public class FactoryDemo
     {
+        /// <summary>
+        /// demo sutiation:
+        ///     given developer using factory pattern to get a instance of class
+        ///     when using app.config to toggle which concreate class should be 
+        ///     then using reflection to resolve the instance of the concreate class
+        /// </summary>
         public static void Demo()
         {
-            string factoryName = ConfigurationSettings.AppSettings["factoryName"];
+            string factoryName = ConfigurationManager.AppSettings["factoryName"]; // need to add reference:system.Configuration
             Console.WriteLine("---------Factory Pattern Demo start-------");
 
-            //var factory=new FileFactory();
+            //var factory=new FileFactory(); 
             var assemblyName = Assembly.GetAssembly(typeof(ILogFactory)).FullName;
-            ILogFactory factory = (ILogFactory)Assembly.Load(assemblyName).CreateInstance(assemblyName+factoryName);
+            var factory = (ILogFactory)Assembly.Load(assemblyName).CreateInstance(factoryName);
             var log = factory.Create();
             log.Write("logging test info");
             Console.WriteLine("---------Factory Pattern Demo end-------\n");
