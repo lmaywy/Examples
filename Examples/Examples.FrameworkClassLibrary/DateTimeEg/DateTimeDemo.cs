1cf324fa058ae813307dcbd433f53a0a78ea85d3
may.li <may.li@bleum.com>
2016-01-04 11:07:06 +0800

    [Dev] Framework class library- adding datetime demo~

---
+++ Examples/Examples.FrameworkClassLibrary/DateTimeEg/DateTimeDemo.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System;
+
+namespace Examples.FrameworkClassLibrary.DateTimeEg
+{
+    public class DateTimeDemo
+    {
+        public static void Demo()
+        {
+            // parse the given format datetime string to datetime type using CurrentCulture 
+            var date = DateTime.ParseExact("05/12/2015", "dd/MM/yyyy", CultureInfo.CurrentCulture);
+        }
+    }
+}
