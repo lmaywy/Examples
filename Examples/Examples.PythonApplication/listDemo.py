9baf009d671a75f5d41039694ab1781766d6a0aa
may.li <may.li@bleum.com>
2016-01-20 18:20:28 +0800

    [Dev] code changes for python

--- Examples/Examples.PythonApplication/listDemo.py
+++ Examples/Examples.PythonApplication/listDemo.py
@@ -1,15 +1,20 @@
 lst=[1,2,3,4,5,6,7]
 # 1.access element by index in order 
+print 'access element by index in order'
 print lst[0];
+print 'traverse element by index in order '
 for x in lst:
     print x;
 
 # 2.access element by index in inverse order
+print 'access element by index in inverse order'
 print lst[-1];
-lenth=len(lst);
+length=len(lst);
 i=length-1;
+print 'traverse element by index in inverse order'
 while(i>=0):
     print lst[i];
+    i=i-1;
 
 # remove an element
 lst.remove(3);
