fc9c95f57b9971efe798e789cfcc497168034898
may <maysaber@126.com>
2016-06-27 20:24:58 +0800

    add build script

---
+++ Examples/Examples.FrameworkClassLibrary/build.ps1
@@ -0,0 +1,2 @@
+
+&  "C:\Git\Examples Git\Examples\Examples.FrameworkClassLibrary\nuget.exe" pack "C:\Git\Examples Git\Examples\Examples.FrameworkClassLibrary\Examples.FrameworkClassLibrary.nuspec" -Version 2.0.0.2 -OutputDirectory "C:\Git\Examples Git"
